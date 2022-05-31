0301c4ee3859a6e4254c99bd44add3a204c89792
getjoojed <103543014+getjoojed@users.noreply.github.com>
2022-05-31 20:48:24 -0300

    Add files via upload

--- src/test/java/calculadora/TestCalculadora.java
+++ src/test/java/calculadora/TestCalculadora.java
@@ -5,14 +5,22 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TestCalculadora {
-    
+
     @Test
-    public void testGetAdicao(){
+    public void testGetAdicao() {
         Calculadora calculadora = new Calculadora(4.0, 2.0);
-        double retornoEsperado  = 6.0;
-        double retornoFeito     = calculadora.getAdicao();
+        double retornoEsperado = 6.0;
+        double retornoFeito = calculadora.getAdicao();
         assertEquals(retornoEsperado, retornoFeito, 0);
-        
+
     }
-    
+
+    @Test
+    public void testGetSubtracao() {
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        double retornoEsperado = 2.0;
+        double retornoFeito = calculadora.getSubtracao();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+
 }
